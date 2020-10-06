bad0f1b26f6cb48840a94b3fbfdb8ee93e8405c0
yusuftml <yusuftemel355@gmail.com>
2020-10-06 21:46:38 +0200

    Second  commit

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,7 @@
+public class AwesomeTest {
+
+    //red mean it's untracked
+    //green mean it's under stating area
+    //grey in .idea folder means it's ignored
+    //when normal mean it's commited
+}
